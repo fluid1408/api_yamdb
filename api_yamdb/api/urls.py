0776c3ad0e43924419bbd1e9c49a78f57c5cd23e
Sergey Shlemin <schlyomin@yandex.ru>
2022-12-20 19:57:07 +0300

    изменение urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,24 +3,18 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
 from api.views import (
-    PostViewSet,
-    FollowViewSet,
-    CommentViewSet,
-    GroupViewSet,
+    TitleViewSet,
+    CategoryViewSet,
+    GenreViewSet,
 )
 
 
 router_v1 = DefaultRouter()
-router_v1.register('posts', PostViewSet, basename='post')
-router_v1.register('follow', FollowViewSet, basename='follow')
-router_v1.register('groups', GroupViewSet, basename='groups')
-router_v1.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register('categories', CategoryViewSet, basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/', include('djoser.urls.jwt')),
 ]
